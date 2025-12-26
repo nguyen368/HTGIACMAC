072f859e2d85e3d02ac325c4f758583c28de88b8
dinhnguyen <anhkl258@gmail.com>
2025-12-26 11:58:49 +0700

    Update: Design Dashboard UI and clean project

--- src/backend/Aura.API/obj/Debug/net8.0/Aura.API.AssemblyInfo.cs
+++ src/backend/Aura.API/obj/Debug/net8.0/Aura.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aura.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f3c1d9810f48d697a51585537f6f8e3bca8698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65a176410cddb3f085ab97135a15d9bea6da785")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aura.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aura.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
